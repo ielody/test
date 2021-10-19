48a64bd2fdb7efe64600899e43f66c9ae8177a1e
Ingeborg Eldøy <ieldoy@gmail.com>
2021-10-19 22:21:13 +0200

    cars,person+date

--- variables.js
+++ variables.js
@@ -1,3 +1,34 @@
 console.log('Hello')
 var hello = 6
 console.log(hello)
+console.log('timeout')
+var a = 3
+var b = 4
+x = a + b
+console.log(x)
+var person = "John Doe",
+carName = "Volvo",
+price = 200
+console.log(person,',', carName,',', price)
+
+const cars = ["Saab", "Volvo", "BMW"]
+console.log(cars)
+cars[1] = "Toyota"
+console.log(cars)
+cars.push("Audi")
+console.log(cars)
+cars[2] = "Audi"
+console.log(cars)
+
+const car = {type:"Fiat", model:"500", color:"white"}
+console.log(car)
+car.color = "red"
+console.log(car.color)
+
+var description = ""
+var person = {firstname:"Isa", lastname:"Ibisa", age:52}
+console.log(description, person)
+
+
+
+
