e04074d3afec249c5625e6f962fddea6251fbbc7
Ingeborg Eldøy <ieldoy@gmail.com>
2021-10-24 22:09:19 +0200

    concat,push

--- array.js
+++ array.js
@@ -1,20 +1,20 @@
-const car = ["Toyota", "Volvo", "BMW"]
-const town = ["Amsterdam", "Berlin", "Paris"]
+//const car = ["Toyota", "Volvo", "BMW"]
+//const town = ["Amsterdam", "Berlin", "Paris"]
 // console.log(car, town)
 // console.log(car[0], town[2])
 
-let array = [4,5,"cake","muffin",7,"cupcake"]
+//let array = [4,5,"cake","muffin",7,"cupcake"]
 //slice
-let newArray = array.slice(2,4)
-// console.log(newArray)
+//let newArray = array.slice(2,4)
+/// console.log(newArray)
 
 //splice-remove
-array.splice(0,2)
-array.splice(2,1)
+// array.splice(0,2)
+// array.splice(2,1)
 // console.log(array)
 
 //splice add
-array.splice(4, 5, 'pain au chocolat', 'cookies')
+// array.splice(4, 5, 'pain au chocolat', 'cookies')
 // console.log(array)
 
 //split string
@@ -59,24 +59,24 @@ array.splice(4, 5, 'pain au chocolat', 'cookies')
 // var sum2 = calculateSum('6,7,8')
 // console.log({ sum2 })
 
-var commaSeparated = '1,2,3,4,5'
+// var commaSeparated = '1,2,3,4,5'
 // console.log({ commaSeparated })
-var commaArray = commaSeparated.split(',')
-console.log({ commaArray })
+// var commaArray = commaSeparated.split(',')
+// console.log({ commaArray })
 
-function mapper(item) {
-  console.log(item)
-  return parseInt(item)
-}
+// function mapper(item) {
+//   console.log(item)
+//   return parseInt(item)
+// }
 
-var numberArray = commaArray.map(mapper)
+// var numberArray = commaArray.map(mapper)
 
 // var numberArray = commaArray.map(function(item) {
 //   console.log(item)
 //   return parseInt(item)
 // })
 
-console.log({ numberArray })
+//console.log({ numberArray })
 
 // var sum = 0
 // numberArray.forEach(function(number) {
@@ -99,13 +99,41 @@ console.log({ numberArray })
 // let kulArray = myString.split(",", 2)
 // console.log(kulArray)
 
-var a = { hei: 'nei', hadet: 'ja' }
+//var a = { hei: 'nei', hadet: 'ja' }
 // Legger a inn i en ny variabel
-var b = a
+//var b = a
 // Endrer på b
-b.hei = 'kanskje'
+//b.hei = 'kanskje'
 // console.log(a)
 
-var longArray = [1,2,3,4,5,6,7,8,9,10]
-var toSlice = longArray.slice(0, 5)
-console.log(toSlice)
+// var longArray = [1,2,3,4,5,6,7,8,9,10]
+// var toSlice = longArray.slice(0, 5)
+//console.log(toSlice)
+
+//concat-merging arrays
+
+/*const array1 = ['a', 'b', 'c'];
+const array2 = ['d', 'e', 'f'];
+const array3 = array1.concat(array2)
+console.log(array3)
+
+const letters = ['sol', 'skinn', 'regn']
+const numbers = [23, 43, 55]
+
+const numletters = letters.concat(numbers)
+console.log(numletters)*/
+
+const num1 = [[1]]
+const num2 = [2, [3]]
+
+const numbers1 = num1.concat(num2)
+//console.log(numbers1)
+
+num1[0].push(65)
+//console.log(numbers1)
+
+num1[0].push(67, 'hai')
+//console.log(numbers1)
+
+num1[0].push('iee', 'yoshi')
+console.log(numbers1)
